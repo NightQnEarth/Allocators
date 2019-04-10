2a93f15772bfae7e5ad0ee52627c1e4660d6eac8
NightQnEarth <Denis1.618@yandex.ru>
2019-04-10 18:06:02 +0500

    Little refactored MemoryBlocksContainer abstract class.

--- MemoryBlocksContainer.h
+++ MemoryBlocksContainer.h
@@ -6,14 +6,13 @@ class MemoryBlocksContainer
 public:
     const int PULL_SIZE = 1024;
 
+    MemoryBlocksContainer() = default;
     virtual ~MemoryBlocksContainer() = default;
+    MemoryBlocksContainer(const MemoryBlocksContainer&) = delete;
+    MemoryBlocksContainer& operator= (const MemoryBlocksContainer&) = delete;
+
     virtual int getMemoryBlockSize() = 0;
     virtual int getMemoryBlockCapacity() = 0;
-
-protected:
-    MemoryBlocksContainer() = default;
-    MemoryBlocksContainer(const MemoryBlocksContainer&) = delete; // NOLINT(hicpp-use-equals-delete,modernize-use-equals-delete)
-    MemoryBlocksContainer& operator= (const MemoryBlocksContainer&) = delete; // NOLINT(hicpp-use-equals-delete,modernize-use-equals-delete)
 };
 
 #endif //ALLOCATORS_MEMORYBLOCKSCONTAINER_H
